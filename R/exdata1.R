200136cb3f43bd4d81755eb08491467a49cdd9bc
Katherine Moore <kmoore@amherst.edu>
2021-11-24 12:14:53 -0500

    Fixing error.

--- R/exdata1.R
+++ R/exdata1.R
@@ -3,11 +3,6 @@
 #' A dataset consisting of 8 points (in 2-dimensional Euclidean space)
 #' for the purposes of example
 #'
-#' @format A data frame with 8 rows and 2 variables:
-#' \describe{
-#'   \item{[, 1]}{x-coordinate}
-#'   \item{[, 2]}{y-coordinate}
-#'   ...
-#' }
+#' @format A data frame with 8 rows and 2 variables.
 #'
 "exdata1"
--- R/exdata2.R
+++ R/exdata2.R
@@ -3,11 +3,7 @@
 #' A dataset consisting of 16 points (in 2-dimensional Euclidean space)
 #' for the purposes of example
 #'
-#' @format A data frame with 16 rows and 2 variables:
-#' \describe{
-#'   \item{[, 1]}{x-coordinate}
-#'   \item{[, 2]}{y-coordinate}
-#'   ...
-#' }
+#' @format A data frame with 16 rows and 2 variables.
+#'
 #'
 "exdata2"

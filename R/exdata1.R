5d6bd34fee182fcf16855c4c70f914cfcde9548c
Katherine Moore <kmoore@amherst.edu>
2021-11-24 12:32:29 -0500

    Editing description.

--- R/exdata1.R
+++ R/exdata1.R
@@ -1,7 +1,8 @@
 #' Example data 1
 #'
 #' A dataset consisting of 8 points (in 2-dimensional Euclidean space)
-#' for the purposes of example
+#'to provide an example.
+#'
 #'
 #' @format A data frame with 8 rows and 2 variables.
 #'
--- R/exdata2.R
+++ R/exdata2.R
@@ -1,7 +1,7 @@
 #' Example data 2
 #'
 #' A dataset consisting of 16 points (in 2-dimensional Euclidean space)
-#' for the purposes of example
+#' to provide an example.
 #'
 #' @format A data frame with 16 rows and 2 variables.
 #'

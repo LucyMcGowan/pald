56058925af7f31f81b36f03d4282be62b40653a0
Katherine Moore <kmoore@amherst.edu>
2021-11-24 11:53:08 -0500

    Adding initial files

---
+++ R/exdata2.R
@@ -0,0 +1,13 @@
+#' Example data 2
+#'
+#' A dataset consisting of 16 points (in 2-dimensional Euclidean space)
+#' for the purposes of example
+#'
+#' @format A data frame with 16 rows and 2 variables:
+#' \describe{
+#'   \item{[, 1]}{x-coordinate}
+#'   \item{[, 2]}{y-coordinate}
+#'   ...
+#' }
+#'
+"exdata2"

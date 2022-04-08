518e0b3873830234cb49719b6c106bb45b50324e
Lucy D'Agostino McGowan <lucydagostino@gmail.com>
2022-04-08 10:22:52 -0400

    remove link

--- R/aggregation.R
+++ R/aggregation.R
@@ -6,5 +6,4 @@
 #' @format A data frame with 788 rows and 2 columns, `x1` and `x2`.
 #' @references A. Gionis, H. Mannila, and P. Tsaparas, Clustering aggregation.
 #' ACM Transactions on Knowledge Discovery from Data (TKDD), 2007. 1(1): p. 1-30.
-#' Available at: \url{http://cs.joensuu.fi/sipu/datasets/}
 "aggregation"
